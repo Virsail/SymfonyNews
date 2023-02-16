aed21b2c996f686d093488b1f454ac4609673b5e
virsail <ericmbagaya@gmail.com>
2023-02-16 19:40:20 +0300

    create a parse news command

---
+++ my-project/src/Command/ParseNewsCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ParseNewsCommand extends Command
+{
+    protected static $defaultName = 'app:parse-news';
+
+    protected function configure()
+    {
+        $this->setDescription('Start parsing news from the Hightload news resource');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Implement the news parsing logic here
+        $output->writeln('Parsing news from Hightload...');
+    }
+}
